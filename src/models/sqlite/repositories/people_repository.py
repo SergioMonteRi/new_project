c42ddfdcfbea2b509e9f9e294d36360a8e690030
SergioMonteRi <sergioribeiropalermo@gmail.com>
2026-09-22 06:08:41 -0300

    feat: create and implement people and pets repositories interfaces

---
+++ src/models/sqlite/repositories/people_repository.py
@@ -0,0 +1,45 @@
+from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
+
+from src.models.sqlite.entities.people import PeopleTable
+from src.models.sqlite.entities.pets import PetsTable
+from src.models.sqlite.interfaces.people_repository import PeopleRepositoryInterface
+from src.models.sqlite.settings.connection import DBConnectionHandler
+
+
+class PeopleRepository(PeopleRepositoryInterface):
+    def __init__(self, db_connection: DBConnectionHandler) -> None:
+        self.__db_connection = db_connection
+
+    def insert_person(
+        self, first_name: str, last_name: str, age: int, pet_id: int
+    ) -> None:
+        with self.__db_connection as session:
+            try:
+                person_data = PeopleTable(
+                    first_name=first_name, last_name=last_name, age=age, pet_id=pet_id
+                )
+
+                session.add(person_data)
+                session.commit()
+
+            except SQLAlchemyError:
+                session.rollback()
+                raise
+
+    def get_person(self, person_id: int):
+        with self.__db_connection as session:
+            stmt = (
+                select(
+                    PeopleTable.first_name,
+                    PeopleTable.last_name,
+                    PetsTable.name.label("pet_name"),
+                    PetsTable.type.label("pet_type"),
+                )
+                .outerjoin(PetsTable, PetsTable.id == PeopleTable.pet_id)
+                .where(PeopleTable.id == person_id)
+            )
+
+            person = session.execute(stmt).one_or_none
+
+            return person
--- src/models/sqlite/repositories/pets_repository.py
+++ src/models/sqlite/repositories/pets_repository.py
@@ -3,14 +3,15 @@ from typing import List
 from sqlalchemy import select
 
 from src.models.sqlite.entities.pets import PetsTable
+from src.models.sqlite.interfaces.pets_repository import PetsRepositoryInterface
 from src.models.sqlite.settings.connection import DBConnectionHandler
 
 
-class PetsRepository:
+class PetsRepository(PetsRepositoryInterface):
     def __init__(self, db_connection: DBConnectionHandler) -> None:
         self.__db_connection = db_connection
 
-    def list_pets(self) -> List:
+    def list_pets(self) -> List[PetsTable]:
         with self.__db_connection as session:
             return session.scalars(select(PetsTable)).all()
 
