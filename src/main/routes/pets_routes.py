26ed908155daf2b9f71f2d7596a81d4417e6f1d4
SergioMonteRi <sergioribeiropalermo@gmail.com>
2026-09-23 10:23:09 -0300

    feat: implement pet list composer and route

--- src/main/routes/pets_routes.py
+++ src/main/routes/pets_routes.py
@@ -1,8 +1,17 @@
 from flask import Blueprint, jsonify
 
+from src.main.composer.pet_list_composer import pet_list_composer
+from src.views.http_types.http_request import HttpRequest
+
 pet_routes_bp = Blueprint("pets_routes", __name__)
 
 
 @pet_routes_bp.route("/pets", methods=["GET"])
 def list_pets():
-    return jsonify({"response": "Pets List"}), 200
+    htpp_request = HttpRequest()
+
+    view = pet_list_composer()
+
+    http_response = view.handle(http_request=htpp_request)
+
+    return jsonify(http_response.body), http_response.status_code
